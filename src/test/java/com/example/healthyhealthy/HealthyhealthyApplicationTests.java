03b7e27ea66325cab2446a68628e37b3f308670d
gpals4431 <gpals4431@naver.com>
2023-11-19 16:48:48 +0900

    test

---
+++ src/test/java/com/example/healthyhealthy/HealthyhealthyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthyhealthy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyhealthyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
