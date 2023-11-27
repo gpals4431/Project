96131bff8ff761339400a509a73312a9d1b38fbc
jirun <jirun0113@naver.com>
2023-11-27 15:02:03 +0900

    Committing local changes

--- src/main/java/com/example/healthyhealthy/util/config/MybatisConfig.java
+++ src/main/java/com/example/healthyhealthy/util/config/MybatisConfig.java
@@ -1,4 +1,4 @@
-package com.example.healthyhealthy.config;
+package com.example.healthyhealthy.util.config;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
--- src/main/webapp/reacthealthy/src/views/join.js
+++ src/main/webapp/reacthealthy/src/views/join.js
@@ -1,275 +1,96 @@
-<!DOCTYPE html>
-<html lang="zxx">
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import '../styles/css/bootstrap.min.css';
+//import '../styles/css/elegant-icons.css';
+import '../styles/css/font-awesome.min.css';
+//import '../styles/css/jquery-ui.min.css';
+//import '../styles/css/nice-select.css';
+// import '../styles/css/owl.carousel.min.css';
+//import '../styles/css/slicknav.min.css';
+import '../styles/css/style.css';
+import { Link, useNavigate } from 'react-router-dom';
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Ogani Template">
-    <meta name="keywords" content="Ogani, unica, creative, html">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ogani | Template</title>
-
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
-
-    <!-- Css Styles -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
-    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
-    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
-    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-
-<body>
-    <!-- Page Preloder -->
-    <div id="preloder">
-        <div class="loader"></div>
-    </div>
-
-    <!-- Header Section Begin -->
-    <header class="header">
-        <div class="header__top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="header__top__left">
-                            <ul>
-                                <li><i class="fa fa-envelope"></i> hello@colorlib.com</li>
-                                <li>Free Shipping for all Order of $99</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="header__top__right">
-                            <div class="header__top__right__social">
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                <a href="#"><i class="fa fa-linkedin"></i></a>
-                                <a href="#"><i class="fa fa-pinterest-p"></i></a>
-                            </div>
-                            <div class="header__top__right__language">
-                                <img src="img/language.png" alt="">
-                                <div>English</div>
-                                <span class="arrow_carrot-down"></span>
-                                <ul>
-                                    <li><a href="#">Spanis</a></li>
-                                    <li><a href="#">English</a></li>
-                                </ul>
-                            </div>
-                            <div class="header__top__right__auth">
-                                <a href="#"><i class="fa fa-user"></i> Login</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="header__logo">
-                        <a href="../../public/index.html"><img src="img/logo.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <nav class="header__menu">
-                        <ul>
-                            <li><a href="../../public/index.html">Home</a></li>
-                            <li class="active"><a href="shop-grid.html">Shop</a></li>
-                            <li><a href="#">Pages</a>
-                                <ul class="header__menu__dropdown">
-                                    <li><a href="shop-details.html">Shop Details</a></li>
-                                    <li><a href="shoping-cart.html">Shoping Cart</a></li>
-                                    <li><a href="checkout.html">Check Out</a></li>
-                                    <li><a href="blog-details.html">Blog Details</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="blog.html">Blog</a></li>
-                            <li><a href="contact.html">Contact</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <div class="col-lg-3">
-                    <div class="header__cart">
-                        <ul>
-                            <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
-                            <li><a href="#"><i class="fa fa-shopping-bag"></i> <span>3</span></a></li>
-                        </ul>
-                        <div class="header__cart__price">item: <span>$150.00</span></div>
-                    </div>
-                </div>
-            </div>
-            <div class="humberger__open">
-                <i class="fa fa-bars"></i>
-            </div>
-        </div>
-    </header>
-    <!-- Header Section End -->
-
-    <!-- Breadcrumb Section Begin -->
-    <section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="breadcrumb__text">
-                        <h2>회원가입</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Breadcrumb Section End -->
-
-    <!-- Checkout Section Begin -->
-    <section class="checkout spad">
-        <div class="container">
-            <div class="checkout__form">
-                <form action="#">
+const JoinForm = () => {
+    return (
+        <>
+            {/* Breadcrumb Section Begin */}
+            <section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
+                <div class="container">
                     <div class="row">
-                        <div class="col-lg-12">
-                            <div class="checkout__input">
-                                <p>아이디 &nbsp;<span>*</span></p>
-                                <input type="text">
-                            </div>
-                            <div class="checkout__input">
-                                <p>비밀번호&nbsp;<span>*</span></p>
-                                <input type="text">
-                            </div>
-                            <div class="checkout__input">
-                                <p>비밀번호 확인&nbsp;<span>*</span></p>
-                                <input type="text" placeholder="Street Address" class="checkout__input__add">
-                            </div>
-                            <div class="checkout__input">
-                                <p>이 름 <span>*</span></p>
-                                <input type="text">
-                            </div>
-                            <div class="checkout__input">
-                                <p>위 치<span>*</span></p>
-                                <input type="text">
-                            </div>
-
-                            <div>
-                                <p style="color: black;">성 별<span style="color: red;">*</span></p>
-                                남<input style="text-align: left;" type="radio" checked>&nbsp;&nbsp;&nbsp;
-                                여<input style="text-align: left;" type="radio">
-                            </div>
-                            <br>
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="checkout__input">
-                                    <p>핸드폰 번호<span>*</span></p>
-                                    <input type="text">
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="checkout__input">
-                                    <p>Email<span>*</span></p>
-                                    <input type="text">
-                                </div>
+                        <div class="col-lg-12 text-center">
+                            <div class="breadcrumb__text">
+                                <h2>회원가입</h2>
                             </div>
                         </div>
-                        <div style="width: 100%; text-align: center;">
-
-                            <button type="submit" class="site-btn" style="text-align: center;">회원가입</button>
-                        </div>
-
-                    </div>
-            </div>
-            </form>
-        </div>
-        </div>
-    </section>
-    <!-- Checkout Section End -->
-
-    <!-- Footer Section Begin -->
-    <footer class="footer spad">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="footer__about">
-                        <div class="footer__about__logo">
-                            <a href="../../public/index.html"><img src="img/logo.png" alt=""></a>
-                        </div>
-                        <ul>
-                            <li>Address: 60-49 Road 11378 New York</li>
-                            <li>Phone: +65 11.188.888</li>
-                            <li>Email: hello@colorlib.com</li>
-                        </ul>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-1">
-                    <div class="footer__widget">
-                        <h6>Useful Links</h6>
-                        <ul>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">About Our Shop</a></li>
-                            <li><a href="#">Secure Shopping</a></li>
-                            <li><a href="#">Delivery infomation</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Our Sitemap</a></li>
-                        </ul>
-                        <ul>
-                            <li><a href="#">Who We Are</a></li>
-                            <li><a href="#">Our Services</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Contact</a></li>
-                            <li><a href="#">Innovation</a></li>
-                            <li><a href="#">Testimonials</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-12">
-                    <div class="footer__widget">
-                        <h6>Join Our Newsletter Now</h6>
-                        <p>Get E-mail updates about our latest shop and special offers.</p>
+            </section>
+            {/* Breadcrumb Section End */}
+
+            {/* Checkout Section Begin */}
+            <section class="checkout spad">
+                <div class="container">
+                    <div class="checkout__form">
                         <form action="#">
-                            <input type="text" placeholder="Enter your mail">
-                            <button type="submit" class="site-btn">Subscribe</button>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="checkout__input">
+                                        <p>아이디 &nbsp;<span>*</span></p>
+                                        <input type="text" />
+                                    </div>
+                                    <div class="checkout__input">
+                                        <p>비밀번호&nbsp;<span>*</span></p>
+                                        <input type="text" />
+                                    </div>
+                                    <div class="checkout__input">
+                                        <p>비밀번호 확인&nbsp;<span>*</span></p>
+                                        <input type="text" placeholder="Street Address" class="checkout__input__add" />
+                                    </div>
+                                    <div class="checkout__input">
+                                        <p>이 름 <span>*</span></p>
+                                        <input type="text" />
+                                    </div>
+                                    <div class="checkout__input">
+                                        <p>위 치<span>*</span></p>
+                                        <input type="text" />
+                                    </div>
+
+                                    <div>
+                                        <p style={{color: 'black'}}>성 별<span style={{color: 'red'}}>*</span></p>
+                                        남<input style={{textAlign: 'left'}} type="radio" checked />&nbsp;&nbsp;&nbsp;
+                                        여<input style={{textAlign: 'left'}} type="radio" />
+                                    </div>
+                                    <br>
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="checkout__input">
+                                                    <p>핸드폰 번호<span>*</span></p>
+                                                    <input type="text" />
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="checkout__input">
+                                                    <p>Email<span>*</span></p>
+                                                    <input type="text" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div style={{width: '100%', text-align: 'center'}}>
+
+                                            <button type="submit" class="site-btn" style="text-align: center;">회원가입</button>
+                                        </div>
+                                    </br>
+
+                                </div>
+                            </div>
                         </form>
-                        <div class="footer__widget__social">
-                            <a href="#"><i class="fa fa-facebook"></i></a>
-                            <a href="#"><i class="fa fa-instagram"></i></a>
-                            <a href="#"><i class="fa fa-twitter"></i></a>
-                            <a href="#"><i class="fa fa-pinterest"></i></a>
-                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="footer__copyright">
-                        <div class="footer__copyright__text">
-                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                Copyright &copy;
-                                <script>document.write(new Date().getFullYear());</script> All rights reserved | This
-                                template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a
-                                    href="https://colorlib.com" target="_blank">Colorlib</a>
-                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            </p>
-                        </div>
-                        <div class="footer__copyright__payment"><img src="img/payment-item.png" alt=""></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- Footer Section End -->
+            </section>
+            {/* Checkout Section End */}
+        </>
+    );
+};
 
-    <!-- Js Plugins -->
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.nice-select.min.js"></script>
-    <script src="js/jquery-ui.min.js"></script>
-    <script src="js/jquery.slicknav.js"></script>
-    <script src="js/mixitup.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/main.js"></script>
-
-
-
-</body>
-
-</html>
+export default JoinForm;
