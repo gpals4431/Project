0a4917506f5c409fad4b67aad841c908b5c6e8c2
gpals4431 <gpals4431@naver.com>
2023-11-27 15:29:29 +0900

    시큐리티 적용 중

--- src/main/java/com/example/healthyhealthy/util/security/SpringSecurityConfig.java
+++ src/main/java/com/example/healthyhealthy/util/security/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -14,12 +15,14 @@ public class SpringSecurityConfig  {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers(new AntPathRequestMatcher("/**"))//모든 요청 허락
-                        .permitAll())
+                .csrf(AbstractHttpConfigurer::disable
+                )
+                .authorizeHttpRequests((authorizeHttpRequests) -> 
+                        authorizeHttpRequests
+                                //모든 요청을 허락
+                            .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
 
-        ;
+                );
         return http.build();
       }
 }
